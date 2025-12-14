c9af97833e8a644c6302cc9315bf52f682f6523b
zqqlove <2456310776@qq.com>
2025-12-15 00:21:27 +0800

    删除dist文件

--- arena-frontend/doudizhu/src/router/index.js
+++ arena-frontend/doudizhu/src/router/index.js
@@ -1,4 +1,3 @@
-// src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router'
 
 // 导入所有页面组件
@@ -13,68 +12,67 @@ import ReplayDetail from '@/views/replay/detail.vue'
 import Paihang from '@/views/paihang/index.vue'
 import Agentcf from '@/views/agentcf/index.vue'
 
-
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
   },
   {
     path: '/home',
     name: 'HomePage',
-    component: Home
+    component: Home,
   },
   {
     path: '/game',
     name: 'Game',
-    component: Game
+    component: Game,
   },
   {
     path: '/login',
     name: 'Login',
-    component: Login
+    component: Login,
   },
   {
     path: '/register',
     name: 'Register',
-    component: Register
+    component: Register,
   },
   {
     path: '/match',
     name: 'Match',
-    component: Match
+    component: Match,
   },
   {
     path: '/matching',
     name: 'Matching',
-    component: Matching
+    component: Matching,
   },
   {
     path: '/replay/list',
     name: 'ReplayList',
-    component: ReplayList
+    component: ReplayList,
   },
   {
     path: '/replay/detail/:id',
     name: 'ReplayDetail',
-    component: ReplayDetail
+    component: ReplayDetail,
   },
   {
     path: '/paihang',
     name: 'Paihang',
-    component: Paihang
+    component: Paihang,
   },
   {
     path: '/agentcf',
     name: 'Agentcf',
-    component: Agentcf
-  }
+    component: Agentcf,
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
 })
 
 export default router
