46d6ea37490268b191a64c5aba0aa78d28b49a14
Junzhe702 <1849199868@qq.com>
2025-12-20 15:34:48 +0800

    feat: 认证闭环

--- arena-backend/main.go
+++ arena-backend/main.go
@@ -4,14 +4,17 @@ import (
 	"arena-backend/config"
 	"arena-backend/model"
 	"arena-backend/router"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	gin.SetMode(gin.ReleaseMode)
 	// ① 初始化数据库（连接 MySQL）
 	config.InitDB()
 
 	// ② 自动建表（如果不存在就创建）
-	config.DB.AutoMigrate(&models.User{})
+	config.DB.AutoMigrate(&model.User{})
 
 	// ③ 初始化路由
 	r := router.SetupRouter()
--- arena-backend/router/router.go
+++ arena-backend/router/router.go
@@ -1,7 +1,8 @@
-package router /*路由*/
+package router
 
 import (
 	"arena-backend/handlers"
+	"arena-backend/middleware"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,10 +10,21 @@ import (
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
 
+	// 信任本机代理（开发环境）
+	r.SetTrustedProxies([]string{"127.0.0.1"})
+
 	api := r.Group("/api/users")
 	{
+		// 公共接口
 		api.POST("/register", handlers.Register)
 		api.POST("/login", handlers.Login)
+
+		// 需要 JWT 的接口
+		auth := api.Group("")
+		auth.Use(middleware.JWTAuth())
+		{
+			auth.GET("/profile", handlers.Profile)
+		}
 	}
 
 	return r
