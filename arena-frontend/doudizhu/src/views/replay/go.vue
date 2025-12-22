c02f237cd24c08022708e8f5676d18b6bfbbc03d
Lzzyxr <1771295000@qq.com>
2025-12-23 01:12:22 +0800

    feat: 添加回放同一游戏集合页和详细页

---
+++ arena-frontend/doudizhu/src/views/replay/go.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="coming-soon">
+    暂未开放，敬请期待！
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ComingSoon'
+}
+</script>
+
+<style scoped>
+.coming-soon {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  font-size: 2.5rem;
+  color: #333;
+  font-weight: 500;
+  letter-spacing: 2px;
+  background-color: #f5f5f5;
+}
+</style>
---
+++ arena-frontend/doudizhu/src/views/replay_detail/texas-poker_detail.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>路由没配</div>
+</template>
